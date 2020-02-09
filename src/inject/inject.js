23ebec37519e8229d79f78cdb2516cb26a45853b
Jiupeng Zhang <jiupeng.zhang@rochester.edu>
2020-02-08 22:27:04 -0500

    options static page done updated inject scripts fine-tuned popup pages

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,20 +1,23 @@
+// red, yellow, green, blue
+const colors = ["#b92929", "#faa828", "#56b947", "#00bcd4"];
+
 function onLoaded() {
     // alert("Inject Done!");
     console.log("Wegasst Inject Done.");
     const items = document.getElementsByClassName("cell");
     for (let i = 0; i < items.length; i++) {
-        items[i].style.border = "4px solid grey";
+        items[i].style.border = "4px solid #999";
     }
-    const itemBtns = document.getElementsByClassName("product-cell-add-to-cart");
-    for (let i = 0; i < itemBtns.length; i++) {
-        itemBtns[i].style.width = "101%";
+    const itemButtons = document.getElementsByClassName("product-cell-add-to-cart");
+    for (let i = 0; i < itemButtons.length; i++) {
+        itemButtons[i].style.width = "101%";
     }
 }
 
 chrome.extension.sendMessage({}, function (response) {
     let readyStateCheckInterval = setInterval(function () {
         let prodCount = document.getElementsByClassName('cell').length;
-        if (document.readyState === "complete" && prodCount > 2) {// && $("div.clearfix").length) {
+        if (document.readyState === "complete" && prodCount > 2) {
             clearInterval(readyStateCheckInterval);
             onLoaded();
         }
